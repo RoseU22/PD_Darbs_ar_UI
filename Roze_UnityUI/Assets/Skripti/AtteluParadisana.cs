ae7d72eb92fdeab7a50198ca9eb8d047d053a810
RozeU <rozeu668@gmail.com>
2023-04-24 16:07:35 +0300

    Speles izveide ar telas izveli un apgerba vilksanu

---
+++ Roze_UnityUI/Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+	public GameObject mainigaisAttels;
+	public Sprite[] atteluMasivs;
+	public GameObject IzmeraSlaideris;
+	public GameObject RotacijasSlaideris;
+
+
+	public void Izkritosais(int indekss)
+	{
+		if(indekss == 0)
+			mainigaisAttels.GetComponent<Image>().sprite 
+			= atteluMasivs[0];
+
+		else if (indekss == 1)
+			mainigaisAttels.GetComponent<Image>().sprite
+			= atteluMasivs[1];
+
+		else if (indekss == 2)
+			mainigaisAttels.GetComponent<Image>().sprite
+			= atteluMasivs[2];
+
+		else if (indekss == 3)
+			mainigaisAttels.GetComponent<Image>().sprite
+			= atteluMasivs[3];
+	}
+
+	public void mainitLielumu()
+	{
+		float pasreizejaisIzmers =
+			IzmeraSlaideris.GetComponent<Slider>().value;
+		mainigaisAttels.transform.localScale =
+			new Vector2(1f* pasreizejaisIzmers,
+				1f*pasreizejaisIzmers);
+	}
+
+	public void MainitRotaciju(){
+		float pasreizejaVertiba = 
+			RotacijasSlaideris.GetComponent<Slider> ().value;
+		mainigaisAttels.transform.localRotation =
+			Quaternion.Euler (0, 0, pasreizejaVertiba * 360);
+	}
+}
---
+++ Roze_UnityUI/Assets/Skripti/ObjektaParvietosana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//importe, lai varetu darboties ar pointer interfeisu
+using UnityEngine.EventSystems;
+
+public class ObjektaParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,
+IDragHandler, IEndDragHandler{
+
+	public Canvas kanva;
+	private RectTransform transformacijaslogs;
+
+	// Use this for initialization
+	void Start () {
+		transformacijaslogs = GetComponent<RectTransform>();
+	}
+
+	public void OnPointerDown(PointerEventData notikums){
+		//Debug.Log("Izdarits kreisais klikskis uz velkama objekta!");
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		//Debug.Log("Uzsakta objekta vilksana!");
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log("Notiek objekta vilksana!");
+		transformacijaslogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		//Debug.Log("Beigta objekta vilksana!");
+	}
+}
