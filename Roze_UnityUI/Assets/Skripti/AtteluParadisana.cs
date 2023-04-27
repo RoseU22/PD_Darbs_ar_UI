5e60a7ac569a2f54a08ff223da09be0faf48b6e3
Roze <rozeu668@gmail.com>
2023-04-27 14:39:22 +0300

    Pievienotas krekla un dzīvnieka opcijas, kā arī opcija viņus paslēpt un parādīt

--- Roze_UnityUI/Assets/Skripti/AtteluParadisana.cs
+++ Roze_UnityUI/Assets/Skripti/AtteluParadisana.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
-	public GameObject mainigaisAttels;
-	public Sprite[] atteluMasivs;
-	public GameObject IzmeraSlaideris;
+    public GameObject mainigaisAttels;
+    public GameObject CepuresmainigaisAttels;
+    public GameObject KreklamainigaisAttels;
+    public GameObject DzivniekamainigaisAttels;
+    public GameObject KrekluAtteli;
+    public GameObject CepuresAttels;
+    public GameObject DzivniekaAttels;
+    public Sprite[] atteluMasivs;
+    public Sprite[] CepuresMasivs;
+    public Sprite[] KreklaMasivs;
+    public Sprite[] DzivniekaMasivs;
+    public GameObject IzmeraSlaideris;
+
+    public void KreklaAtteli(bool vertiba)
+    {
+        KrekluAtteli.SetActive(vertiba);
+    }
+
+    public void CepuresAtteli(bool vertiba)
+    {
+        CepuresAttels.SetActive(vertiba);
+    }
+
+    public void DzivniekaAtteli(bool vertiba)
+    {
+        DzivniekaAttels.SetActive(vertiba);
+    }
 
 
-	public void Izkritosais(int indekss)
+    public void Izkritosais(int indekss)
 	{
 		if(indekss == 0)
 			mainigaisAttels.GetComponent<Image>().sprite 
@@ -29,7 +53,68 @@ public class AtteluParadisana : MonoBehaviour {
 			= atteluMasivs[3];
 	}
 
-	public void mainitLielumu()
+    public void CepuresIzkritosais(int indekss)
+    {
+        if (indekss == 0)
+            CepuresmainigaisAttels.GetComponent<Image>().sprite
+            = CepuresMasivs[0];
+
+        else if (indekss == 1)
+            CepuresmainigaisAttels.GetComponent<Image>().sprite
+            = CepuresMasivs[1];
+
+        else if (indekss == 2)
+            CepuresmainigaisAttels.GetComponent<Image>().sprite
+            = CepuresMasivs[2];
+
+        else if (indekss == 3)
+            CepuresmainigaisAttels.GetComponent<Image>().sprite
+            = CepuresMasivs[3];
+
+        else if (indekss == 4)
+            CepuresmainigaisAttels.GetComponent<Image>().sprite
+            = CepuresMasivs[4];
+    }
+
+    public void KreklaIzkritosais(int indekss)
+    {
+        if (indekss == 0)
+            KreklamainigaisAttels.GetComponent<Image>().sprite
+            = KreklaMasivs[0];
+
+        else if (indekss == 1)
+            KreklamainigaisAttels.GetComponent<Image>().sprite
+            = KreklaMasivs[1];
+
+        else if (indekss == 2)
+            KreklamainigaisAttels.GetComponent<Image>().sprite
+            = KreklaMasivs[2];
+
+        else if (indekss == 3)
+            KreklamainigaisAttels.GetComponent<Image>().sprite
+            = KreklaMasivs[3];
+    }
+
+    public void DzivniekaIzkritosais(int indekss)
+    {
+        if (indekss == 0)
+            DzivniekamainigaisAttels.GetComponent<Image>().sprite
+            = DzivniekaMasivs[0];
+
+        else if (indekss == 1)
+            DzivniekamainigaisAttels.GetComponent<Image>().sprite
+            = DzivniekaMasivs[1];
+
+        else if (indekss == 2)
+            DzivniekamainigaisAttels.GetComponent<Image>().sprite
+            = DzivniekaMasivs[2];
+
+        else if (indekss == 3)
+            DzivniekamainigaisAttels.GetComponent<Image>().sprite
+            = DzivniekaMasivs[3];
+    }
+
+    public void mainitLielumu()
 	{
 		float pasreizejaisIzmers = IzmeraSlaideris.GetComponent<Slider>().value;
 		mainigaisAttels.transform.localScale = new Vector2(1f* pasreizejaisIzmers, 1f*pasreizejaisIzmers);
